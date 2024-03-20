b8e90e71e6fe4441d8871790dd8aa506170b5f49
JOSSANTOS-12 <agegnehuyosef1205@gmail.com>
2024-03-20 23:52:05 +0300

    Now I add Header CSS

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import'./header.css'
+function Header() {
+    return (
+        <div>
+            <div className='outer-container'>
+                <ul className='list-container'>
+                    <li>Netflix
+                    </li>
+                    <li>home</li>
+                    <li>contact</li>
+                    <li>About</li>
+                </ul>
+                
+            </div>
+    </div>
+    )
+}
+
+export default Header
