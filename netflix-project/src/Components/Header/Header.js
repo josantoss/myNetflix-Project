947e01673145bee33e50a93716a455917cf84cc1
JOSSANTOS-12 <agegnehuyosef1205@gmail.com>
2024-03-22 02:55:20 +0300

    I add Footer

--- netflix-project/src/Components/Header/Header.js
+++ netflix-project/src/Components/Header/Header.js
@@ -3,14 +3,20 @@ import'./header.css'
 function Header() {
     return (
         <div>
-            <div className="outer-container">
-            <ul className="list-container">
-                <li>Netflix</li>
-                <li>home</li>
-                <li>contact</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+            <div className="Header-outer-container">
+                <div className="Header-container">
+                    <div className="Header-left-container">
+                        <ul className="list-container">
+                            <li>Netflix</li>
+                            <li>home</li>
+                            <li>contact</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                    <div className="Header-right-container">
+                    </div>
+                </div>
             </div>
         </div>
     );
