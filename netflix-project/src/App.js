52e093e2ced582bbb689b33f12f86400cbf4e468
JOSSANTOS-12 <agegnehuyosef1205@gmail.com>
2024-04-01 00:52:05 +0300

    I add another modifcation

--- netflix-project/src/App.js
+++ netflix-project/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+// import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
+      <h1>This is react App</h1>
       <Home />
     </div>
   );
