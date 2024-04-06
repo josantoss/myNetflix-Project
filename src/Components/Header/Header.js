0e566727d4d07d7d7953b8970d2a851c392989f0
JOSSANTOS-12 <agegnehuyosef1205@gmail.com>
2024-04-07 00:22:43 +0300

    I add github-pages

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,38 +9,38 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 const Header = () => {
     return (
         <div className="header_outer_container">
-        <div className="header_container">
-            <div className="header_left">
-            <ul>
-                <li>
-                <img src={NetflixLogo} alt="Netflix Logo" width="100" />
-                </li>
-                {/* <li>Netflix</li> */}
-                <li>Home</li>
-                <li>TVShows</li>
-                <li>Movies</li>
-                <li>Latest</li>
-                <li>MyList</li>
-                <li>Browse by Languages</li>
-            </ul>
+            <div className="header_container">
+                <div className="header_left">
+                    <ul>
+                        <li>
+                        <img src={NetflixLogo} alt="Netflix Logo" width="50" />
+                        </li>
+                        {/* <li>Netflix</li> */}
+                        <li>Home</li>
+                        <li>TVShows</li>
+                        <li>Movies</li>
+                        <li>Latest</li>
+                        <li>MyList</li>
+                        <li>Browse by Languages</li>
+                    </ul>
+                </div>
+                <div className="header_right">
+                    <ul>
+                        <li>
+                        <SearchIcon />
+                        </li>
+                        <li>
+                        <NotificationsNoneIcon />
+                        </li>
+                        <li>
+                        <AccountBoxIcon />
+                        </li>
+                        <li>
+                        <ArrowDropDownIcon />
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className="header_right">
-            <ul>
-                <li>
-                <SearchIcon />
-                </li>
-                <li>
-                <NotificationsNoneIcon />
-                </li>
-                <li>
-                <AccountBoxIcon />
-                </li>
-                <li>
-                <ArrowDropDownIcon />
-                </li>
-            </ul>
-            </div>
-        </div>
         </div>
     );
     };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Banner from "../../Components/Banner/Banner";
-import RowList from "../../Components/Rows/RowList/RowList.js";
+import RowList from "../../Components/Rows/RowList/RowList";
 
 const Home = () => {
     return (
@@ -13,6 +13,6 @@ const Home = () => {
         <Footer />
         </>
     );
-    };
+};
 
 export default Home;
